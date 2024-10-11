d48cdedc3c337b5034ba7476747593a3b7f7d6e9
ttf416 <thefirstfruit416@gmail.com>
2024-10-11 15:05:56 +0900

    components 완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-// Header component
-
+import Image from "next/image";
+import MsgIcon from "public/icons/msg.svg";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between w-full h-[120px] px-[70px] py-[25px] bg-[#C73820]">
+      <div className="flex gap-[15px] ">
+        <Image src={MsgIcon} alt="편지 아이콘" />
+        <h2 className="w-[280px] h-[40px] text-white text-[50px] text-center font-['Saemaeul-R']">
+          연말 편지 우체통
+        </h2>
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default Header;
--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,7 +1,9 @@
 // MessageBox component
 
 const MessageBox = () => {
-  return <div></div>;
+  return (
+    <div className="w-[400px] h-[300px] px-20 py-12 border-[10px] bg-white border-[#C73820] rounded-[40px]"></div>
+  );
 };
 
 export default MessageBox;
