c9af92d058725b94e28ed26f98ca6e77173687bd
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-11-03 22:07:34 +0900

    Revert "4주차 과제"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 // 아래의 import문으로 next/image의 Image 컴포넌트를 사용할 수 있습니다.
- import Image from "next/image"
+// import Image from "next/image"
 
 // 아래의 import문으로 MessageBox 컴포넌트를 사용할 수 있습니다.
-import MessageBox from "@/components/MessageBox";
+//import MessageBox from "@/components/MessageBox";
 
 export default function Home() {
   return (
