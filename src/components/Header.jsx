49370c61f76edb632bff31d174018e7976496597
us2xince <kstar1029@ewhain.net>
2024-11-03 12:52:42 +0900

    4주차 과제

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 // Header component
+import React from 'react';
+import "./global.css";
 
 const Header = () => {
-  return <div></div>;
+  return <div>
+   <header className="header">
+    <div className="title">연말편지 우체통</div>
+    <button className="button">Google 로그인하고 쓰기</button>
+   </header>
+    
+  </div>;
 };
 
 export default Header;
