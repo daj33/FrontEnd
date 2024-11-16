36351de80d81bf290071eb566313bce177f269ea
ttf416 <thefirstfruit416@gmail.com>
2024-11-16 23:41:55 +0900

    김다지

---
+++ public/API.jsx
@@ -0,0 +1,2 @@
+const api = "http://ec2-3-38-49-253.ap-northeast-2.compute.amazonaws.com:8080/"
+export default api
--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,10 +1,14 @@
 // MessageBox component
 
-const MessageBox = ({ message, name }) => {
+const MessageBox = (children) => {
   return (
-    <div className="w-[400px] h-[300px] px-20 py-12 border-[10px] bg-white border-[#C73820] rounded-[40px]">
-      <p>{JSON.stringify(message)}</p>
-      <p>From. {JSON.stringify(name)}</p>
+    <div className="w-[400px] h-[300px] border-[10px] bg-white border-[#C73820] rounded-[40px]">
+      <div className="w-[350px] h-[250px]flex-col mx-[25px] my-[21px] gap-[2px]">
+        <div className="w-[350px] h-[220px]">{children.message}</div>
+        <div className="w-[135px] h-[36px] place-content-end text-end ">
+          From. {children.name}
+        </div>
+      </div>
     </div>
   );
 };
